a04b2f58def241398cd8b001955529ee97b9e398
Sujal <sujalthummar23@gmail.com>
2024-12-24 18:42:25 +0530

    `Updated resume data with alternating summary descriptions and commented out Node.js, MongoDB, PostgreSQL, and MySQL skills.`

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,10 +10,10 @@ export const DATA = {
   description:
     // "Software Engineer turned Entrepreneur. I love building things and helping people",
     "Software Engineer. I love building things and helping people",
-  summary:
-    "Mastering React, Next.js, and Node.js empowers you as a web developer to build dynamic and scalable solutions. By integrating TypeScript and advanced architectural patterns, you can create efficient, maintainable code that stands up to the demands of modern development. This expertise allows you to tackle complex challenges, optimize performance, and stay ahead of industry trends, making you a valuable contributor to any development team in the ever-evolving web development landscape.",
   // summary:
-  //   "Mastering React and Next.js empowers you as a web developer to build dynamic and scalable solutions. By integrating TypeScript and advanced architectural patterns, you can create efficient, maintainable code that stands up to the demands of modern development. This expertise allows you to tackle complex challenges, optimize performance, and stay ahead of industry trends, making you a valuable contributor to any development team in the ever-evolving web development landscape.",
+  //   "Mastering React, Next.js, and Node.js empowers you as a web developer to build dynamic and scalable solutions. By integrating TypeScript and advanced architectural patterns, you can create efficient, maintainable code that stands up to the demands of modern development. This expertise allows you to tackle complex challenges, optimize performance, and stay ahead of industry trends, making you a valuable contributor to any development team in the ever-evolving web development landscape.",
+  summary:
+    "Mastering React and Next.js empowers you as a web developer to build dynamic and scalable solutions. By integrating TypeScript and advanced architectural patterns, you can create efficient, maintainable code that stands up to the demands of modern development. This expertise allows you to tackle complex challenges, optimize performance, and stay ahead of industry trends, making you a valuable contributor to any development team in the ever-evolving web development landscape.",
   avatarUrl: "/me1.jpeg",
   skills: [
     "HTML5",
@@ -23,10 +23,10 @@ export const DATA = {
     "Redux",
     "Typescript",
     "Next.js",
-    "Node.js",
-    "MongoDB",
-    "PostgreSQL",
-    "MySQL",
+    // "Node.js",
+    // "MongoDB",
+    // "PostgreSQL",
+    // "MySQL",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
